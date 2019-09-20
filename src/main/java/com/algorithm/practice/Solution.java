8c501503e01c259dc22dc907996cd4af838cf209
stella lim <apple3637@gmail.com>
2019-09-20 20:26:24 +0900

    home

--- src/main/java/com/algorithm/practice/Solution.java
+++ src/main/java/com/algorithm/practice/Solution.java
@@ -1,65 +1,45 @@
 package com.algorithm.practice;
 
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Scanner;
 
 public class Solution {
 
 	public static void main(String[] args) {
 		
-		System.out.println(solution());
-
+		//System.out.println(solution());
+		solution();
 	}
 	
 	
-	static public String solution() {
+	static public void solution() {
 		
-		char[] arr1 = new char[100];
-		char[] arr2 = new char[100];
+		Scanner sc = new Scanner(System.in);
+		
 		HashMap<String,Integer> map1 = new HashMap<String,Integer>();
-		HashMap<String,Integer> map2 = new HashMap<String,Integer>();
-		HashMap<String,Integer> result = new HashMap<String,Integer>();
 		
 		int max = 0;
 		String keyname = "";
 		
-		String s1= "penpineapple";
-		String s2 ="applepen";
+		String s1= sc.next();
+		String s2 =sc.next();
 		
-		arr1 = s1.toCharArray();
-		arr2 = s2.toCharArray();
-		String st1="";
-		String st2 ="";
 		
 		for(int i=0; i<s1.length();i++) {
-			for(int j=0; j<i;j++) {
-				 st1 = st1+arr1[j];
-				 st2 = st2+arr2[j];
+			for(int j=i+1; j<=s1.length();j++) {
+				 map1.put(s1.substring(i,j), (j+1-i));
 			}
-			//map1.put(st1, !map1.get(st1).equals(null)?map1.get(st1)+1:0);
-			//map1.put(st2, !map1.get(st2).equals(null)?map2.get(st2)+1:0);
-			
-			map1.put(st1, 1);
-			map1.put(st2, 1);
-			
-			System.out.println(st1+","+map1.get(st1));
-			
-			st1="";
-			st2="";
 		}
-		
-		for( String key : map1.keySet() )
-		{ 
-			if(map1.get(key) == map2.get(key)) {
-				result.put(key, result.get(key)+1);
-				if(max<result.get(key)) {
-					keyname = key;
-					max=result.get(key);
+
+		for(int i=0; i<s2.length();i++) {
+			for(int j=i+1; j<=s2.length();j++) {
+				if(map1.containsKey(s2.substring(i, j)) && max<map1.get(s2.substring(i,j))) {
+					keyname = s2.substring(i,j);
+					max = map1.get(s2.substring(i,j));
 				}
 			}
 		}
-		
-		return keyname;
+	System.out.println(keyname);
 	}
 
 }
